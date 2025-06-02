74b2d2ff639b37cb3cc165accd0aec0a926eaf8e
Ethel Isamar Damián Inoñán <145041698+ethel1312@users.noreply.github.com>
2025-06-02 01:50:49 -0500

    Revert "Agregar domicilio"

--- app/src/main/java/com/example/tienda_ropa/model/EmailReq.java
+++
@@ -1,9 +0,0 @@
-package com.example.tienda_ropa.model;
-
-public class EmailReq {
-    private String email;
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
--- app/src/main/java/com/example/tienda_ropa/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tienda_ropa/ui/home/HomeFragment.java
@@ -1,7 +1,5 @@
 package com.example.tienda_ropa.ui.home;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +7,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.tienda_ropa.MainActivity;
 import com.example.tienda_ropa.databinding.FragmentHomeBinding;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
 
@@ -30,33 +24,10 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Obtener el nombre directamente desde Firebase
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String nombre = (user != null && user.getDisplayName() != null)
-                ? user.getDisplayName()
-                : "Usuario";
-
-        // Mostrar en el TextView
-        binding.textHome.setText("Bienvenido, " + nombre);
-
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
-
-    // para el nombre en el inicio
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        SharedPreferences prefs = requireActivity()
-                .getSharedPreferences("user_session", Context.MODE_PRIVATE);
-        String username = prefs.getString("username", "Usuario");
-        String saludo = "Hola, " + username;
-        AppCompatActivity act = (AppCompatActivity) getActivity();
-        if (act != null && act.getSupportActionBar() != null) {
-            act.getSupportActionBar().setTitle(saludo);
-        }
-    }
-
 
     @Override
     public void onDestroyView() {
